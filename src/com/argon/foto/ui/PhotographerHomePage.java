3ce7c512a8291396938dbea634c61f55ac9a6bb1
Sean <yuan.xu@visionure.com>
2014-08-18 16:35:54 +0800

    Add Name and follow button Symptom: Root Cause: Solution: Project: Self-Test Status: Note: N/A

--- src/com/argon/foto/ui/PhotographerHomePage.java
+++ src/com/argon/foto/ui/PhotographerHomePage.java
@@ -2,6 +2,7 @@ package com.argon.foto.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.argon.foto.R;
 
@@ -10,5 +11,17 @@ public class PhotographerHomePage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photographer_home);
+        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PhotographerHomePage.this.finish();
+            }
+        });
+        findViewById(R.id.btn_share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: to be implemented
+            }
+        });
     }
 }

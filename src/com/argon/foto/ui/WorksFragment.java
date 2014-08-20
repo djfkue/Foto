8977cb602054628b169b9525087db6b9d7a578ec
Sean <yuan.xu@visionure.com>
2014-08-20 11:20:43 +0800

    Add demo album cover Symptom: Root Cause: Solution: Project: Self-Test Status: Note: N/A

--- src/com/argon/foto/ui/WorksFragment.java
+++ src/com/argon/foto/ui/WorksFragment.java
@@ -1,7 +1,73 @@
 package com.argon.foto.ui;
 
+import java.util.ArrayList;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.argon.foto.R;
 
 public class WorksFragment extends Fragment {
+    private ArrayList<Integer> mWorksDataSet = new ArrayList<Integer>();
+    private GridView mWorks;
+    private BaseAdapter mWorksAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return mWorksDataSet.size();
+        }
 
+        @Override
+        public Object getItem(int pos) {
+            return mWorksDataSet.get(pos);
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.works_album_cover, parent, false);
+            }
+            ImageView cover = (ImageView) convertView;
+            cover.setImageResource(mWorksDataSet.get(pos));
+            return convertView;
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_works, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mWorks = (GridView)getView().findViewById(R.id.works);
+        mWorks.setAdapter(mWorksAdapter);
+        mock_loadWorks();
+    }
+
+    private void mock_loadWorks() {
+        mWorksDataSet.clear();
+        mWorksDataSet.add(R.drawable.cover_01);
+        mWorksDataSet.add(R.drawable.cover_02);
+        mWorksDataSet.add(R.drawable.cover_03);
+        mWorksDataSet.add(R.drawable.cover_04);
+        mWorksAdapter.notifyDataSetChanged();
+    }
 }
